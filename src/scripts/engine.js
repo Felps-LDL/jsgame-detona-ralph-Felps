4b5300d976a4d2f44fe1b540ebdf87c164881009
Felps_LDL <felipe.lages.lima@gmail.com>
2025-07-21 18:34:08 -0300

    Tempo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,7 +9,7 @@ const state = {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
-    curretTime: 60,
+    curretTime: 50,
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
